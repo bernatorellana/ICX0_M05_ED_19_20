b60ec2257bc5dd9f88495b45cd2258f70d2694c1
Bernat <bernat.orellana@gmail.com>
2019-10-04 17:19:07 +0200

    repo

---
+++ 3_compilacio_java_manual/source/compila.bat
@@ -0,0 +1,7 @@
+
+set PATH="C:\Program Files\Java\jdk1.8.0_172\bin";%PATH%
+
+javac.exe edu\iesmila\ed\HolaMon.java
+
+jar -cvf HolaMon.jar .
+pause
---
+++ 3_compilacio_java_manual/source/executa.bat
@@ -0,0 +1,6 @@
+
+set PATH="C:\Program Files\Java\jdk1.8.0_172\bin";%PATH%
+
+
+java.exe edu.iesmila.ed.HolaMon
+pause
