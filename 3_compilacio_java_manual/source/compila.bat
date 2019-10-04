417d7c8e8ac7751c472a41ff917cbf31e219f8ba
Bernat <bernat.orellana@gmail.com>
2019-10-04 17:44:36 +0200

    exercici llibreries

--- 3_compilacio_java_manual/source/compila.bat
+++ 3_compilacio_java_manual/source/compila.bat
@@ -3,5 +3,5 @@ set PATH="C:\Program Files\Java\jdk1.8.0_172\bin";%PATH%
 
 javac.exe edu\iesmila\ed\HolaMon.java
 
-jar -cvf HolaMon.jar .
+jar -cfm HolaMon.jar MANIFEST.MF .
 pause
--- 3_compilacio_java_manual/source/executa.bat
+++ 3_compilacio_java_manual/source/executa.bat
@@ -1,6 +1,10 @@
 
 set PATH="C:\Program Files\Java\jdk1.8.0_172\bin";%PATH%
 
+rem Execució directa sobre les classes
+java edu.iesmila.ed.HolaMon
 
-java.exe edu.iesmila.ed.HolaMon
+rem Execució directa sobre el jar (executar el jar)
+java -jar HolaMon.jar
+
 pause
