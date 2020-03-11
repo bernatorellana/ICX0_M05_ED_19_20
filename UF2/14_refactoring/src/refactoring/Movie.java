92d74482643ea36f1f13ed6797818ccd11994076
Usuari <Usuari@IN02>
2020-03-11 16:42:36 +0100

    Moure el càlcul de preu a Movie

---
+++ UF2/14_refactoring/src/refactoring/Movie.java
@@ -0,0 +1,56 @@
+package refactoring;
+
+// From book: 'Refactoring' by Martin Fowler
+// This is the original code before refactoring begins
+public class Movie {
+
+    public static final int CHILDRENS = 2;
+    public static final int NEW_RELEASE = 1;
+    public static final int REGULAR = 0;
+
+    private String _title;
+    private int _priceCode;
+
+    public Movie(String title, int priceCode) {
+        _title = title;
+        _priceCode = priceCode;
+    }
+
+    public int getPriceCode() {
+        return _priceCode;
+    }
+
+    public void setPriceCode(int arg) {
+        _priceCode = arg;
+    }
+
+    public String getTitle() {
+        return _title;
+    }
+
+    double getPrice(int daysRented) {
+
+        double amount = 0;
+
+        //determine amounts for each line
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                amount = 2;
+                if (daysRented > 2) {
+                    amount += (daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                amount = daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                amount = 1.5;
+                if (daysRented > 3) {
+                    amount += (daysRented - 3) * 1.5;
+                }
+                break;
+        }
+        return amount;
+    }
+
+}
