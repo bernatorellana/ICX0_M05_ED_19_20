6782a1307c75e22f70f75a97343301e2c8f23215
Usuari <Usuari@IN02>
2020-03-11 16:58:38 +0100

    herència per substituir el switch

--- UF2/14_refactoring/src/refactoring/Movie.java
+++ UF2/14_refactoring/src/refactoring/Movie.java
@@ -2,7 +2,7 @@ package refactoring;
 
 // From book: 'Refactoring' by Martin Fowler
 // This is the original code before refactoring begins
-public class Movie {
+public abstract class Movie {
 
     public static final int CHILDRENS = 2;
     public static final int NEW_RELEASE = 1;
@@ -28,29 +28,6 @@ public class Movie {
         return _title;
     }
 
-    double getPrice(int daysRented) {
-
-        double amount = 0;
-
-        //determine amounts for each line
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                amount = 2;
-                if (daysRented > 2) {
-                    amount += (daysRented - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                amount = daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                amount = 1.5;
-                if (daysRented > 3) {
-                    amount += (daysRented - 3) * 1.5;
-                }
-                break;
-        }
-        return amount;
-    }
-
+    abstract double getPrice(int daysRented);
+ 
 }
